9c0ad9e52c6437e334ecd80616d54cb17234a10f
Tamás Gálffy <ezittgtx@gmail.com>
2024-11-09 12:51:33 +0100

    implement simple scene

---
+++ public/js/pixijs.js
@@ -0,0 +1,56 @@
+// import * as PIXI from 'pixi.js'
+
+function range(n) {
+  return [...new Array(n)].map((_, i) => i)
+}
+
+// Init
+const app = new PIXI.Application()
+await app.init({ width: 256, height: 256 })
+
+document.body.appendChild(app.canvas)
+
+// Load resources
+const assets = {
+  necromant: await PIXI.Assets.load('/assets/necromant.png'),
+  background: await PIXI.Assets.load('/assets/test-background.png')
+}
+
+// Setup scene
+const background = PIXI.Sprite.from(assets.background)
+app.stage.addChild(background)
+
+const avatarCount = 16
+const avatarMoveSpeed = 1
+const avatars = range(avatarCount).map(() => {
+  const avatar = PIXI.Sprite.from(assets.necromant)
+  
+  avatar.position.set(
+    Math.random() * app.canvas.width,
+    Math.random() * app.canvas.height
+  )
+
+  avatar.pivot.set(avatar.width / 2, avatar.height / 2)
+
+  return avatar
+})
+
+avatars.forEach(avatar => app.stage.addChild(avatar))
+
+// Animate
+app.ticker.add(ticker => {
+  avatars.forEach(avatar => {
+    const brownian = new PIXI.Point(
+      2. * Math.random() - 1.,
+      2. * Math.random() - 1.
+    )
+    .normalize()
+
+    const velocity = brownian.multiplyScalar(ticker.deltaTime * avatarMoveSpeed)
+
+    avatar.position.x += velocity.x
+    avatar.position.y += velocity.y
+  })
+})
+
+app.start()
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 Bun.serve({
-  fetch(req: Request) {
+  async fetch(req: Request) {
     const url = new URL(req.url)
-    if (url.pathname === '/pixijs')
-      return new Response(Bun.file('view/pixijs.html'))
 
-    return new Response('/')
+    // Fixed routes
+    if (url.pathname === '/pixijs')
+      return new Response(Bun.file('public/pixijs.html'))
+
+    // Asset routes
+    if (url.pathname.startsWith('/assets/'))
+      return new Response(Bun.file(url.pathname.slice(1)))
+
+    // Public routes
+    const publicFile = Bun.file('public' + url.pathname)
+    console.log('Trying public path', publicFile.name)
+    if (await publicFile.exists())
+      return new Response(publicFile)
+
+    return new Response('?')
   }
 })
