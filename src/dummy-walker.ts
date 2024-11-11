1f4337ea5c89c54d7b5d1b66c822c95b2395c33b
Faragó Tamás <ocsi0520@gmail.com>
2024-11-11 09:39:13 +0100

    SNAPSHOT: add eslint config

--- src/dummy-walker.ts
+++ src/dummy-walker.ts
@@ -1,90 +1,90 @@
-const EPSILON = 0.000_001
+const EPSILON = 0.000_001;
 
 class vec2 {
-  public x: number = 0
-  public y: number = 0
+  public x: number = 0;
+  public y: number = 0;
 
   constructor(x?: number, y?: number) {
-    this.x = x ?? 0
-    this.y = y ?? 0
+    this.x = x ?? 0;
+    this.y = y ?? 0;
   }
 
   clone(): vec2 {
-    return new vec2(this.x, this.y)
+    return new vec2(this.x, this.y);
   }
 
   distanceTo(p: vec2): number {
     return Math.sqrt(
       Math.pow(this.x - p.x, 2) +
       Math.pow(this.y - p.y, 2)
-    )
+    );
   }
 
   directionTo(p: vec2): vec2 {
-    return this.vectorTo(p).normalize()
+    return this.vectorTo(p).normalize();
   }
 
   vectorTo(p: vec2): vec2 {
     return new vec2(
       p.x - this.x,
       p.y - this.y
-    )
+    );
   }
 
   dot(p: vec2): number {
-    return this.x * p.x + this.y * p.y
+    return this.x * p.x + this.y * p.y;
   }
 
   length(): number {
-    return Math.sqrt(this.dot(this))
+    return Math.sqrt(this.dot(this));
   }
 
-  add(p: vec2): vec2 {
-    this.x += p.x
-    this.y += p.y
+  add(p: vec2): this {
+    this.x += p.x;
+    this.y += p.y;
 
-    return this
+    return this;
   }
 
-  subtract(p: vec2): vec2 {
-    this.x -= p.x
-    this.y -= p .y
+  subtract(p: vec2): this {
+    this.x -= p.x;
+    this.y -= p .y;
 
-    return this
+    return this;
   }
 
-  scale(f: number): vec2 {
-    this.x *= f
-    this.y *= f
+  scale(f: number): this {
+    this.x *= f;
+    this.y *= f;
 
-    return this
+    return this;
   }
 
-  scaleTo(d: number): vec2 {
-    const l = this.length()
+  scaleTo(d: number): this {
+    const l = this.length();
     if (l > EPSILON) {
-      this.x *= d / l
-      this.y *= d / l
+      this.x *= d / l;
+      this.y *= d / l;
     }
 
-    return this
+    return this;
   }
 
-  normalize(): vec2 {
-    const l = this.length()
+  normalize(): this {
+    const l = this.length();
     if (l > EPSILON) {
-      this.x /= l
-      this.y /= l
+      this.x /= l;
+      this.y /= l;
     }
 
-    return this
+    return this;
   }
 
   static randomDirection(): vec2 {
     return new vec2(
       2 * Math.random() - 1,
       2 * Math.random() - 1
-    ).normalize()
+    ).normalize();
   }
 }
 
@@ -103,27 +103,27 @@ export default class DummyWalker<T> {
 
   update(dt: number): void {
     // Calculate weights
-    let cohesionWeight = this.position.distanceTo(this.target) / 96
-    cohesionWeight = Math.min(1., Math.max(0., cohesionWeight))
-    cohesionWeight = Math.pow(cohesionWeight, 4.)
+    let cohesionWeight = this.position.distanceTo(this.target) / 96;
+    cohesionWeight = Math.min(1., Math.max(0., cohesionWeight));
+    cohesionWeight = Math.pow(cohesionWeight, 4.);
 
-    const brownianWeight = 1 - cohesionWeight
+    const brownianWeight = 1 - cohesionWeight;
 
     // Calculate forces
-    const brownianForce = vec2.randomDirection().scale(brownianWeight)
+    const brownianForce = vec2.randomDirection().scale(brownianWeight);
 
     const cohesionForce = this.position.directionTo(this.target)
-      .scale(cohesionWeight)
+      .scale(cohesionWeight);
 
     const acceleration = new vec2()
       .add(cohesionForce)
       .add(brownianForce)
-      .scale(this.accelSpeed * dt)
+      .scale(this.accelSpeed * dt);
 
-    this.velocity.add(acceleration)
+    this.velocity.add(acceleration);
     if (this.velocity.length() > this.moveSpeed)
-      this.velocity.scaleTo(this.moveSpeed)
+      this.velocity.scaleTo(this.moveSpeed);
 
-    this.position.add(this.velocity.clone().scale(dt))
+    this.position.add(this.velocity.clone().scale(dt));
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 Bun.serve({
   async fetch(req: Request) {
-    const url = new URL(req.url)
+    const url = new URL(req.url);
 
     // Fixed routes
     if (url.pathname === '/pixijs')
-      return new Response(Bun.file('public/pixijs.html'))
+      return new Response(Bun.file('public/pixijs.html'));
 
     // Asset routes
     if (url.pathname.startsWith('/assets/'))
-      return new Response(Bun.file(url.pathname.slice(1)))
+      return new Response(Bun.file(url.pathname.slice(1)));
 
     // Public routes
-    const publicFile = Bun.file('public' + url.pathname)
-    console.log('Trying public path', publicFile.name)
+    const publicFile = Bun.file('public' + url.pathname);
+    console.log('Trying public path', publicFile.name);
     if (await publicFile.exists())
-      return new Response(publicFile)
+      return new Response(publicFile);
 
-    return new Response('?')
+    return new Response('?');
   }
-})
+});
