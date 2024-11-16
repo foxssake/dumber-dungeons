53fcbc1e0765208e195d4a3a09695c68bfebc008
Faragó Tamás <ocsi0520@gmail.com>
2024-11-16 11:40:03 +0100

    SNAPSHOT: add DummyWalker into phaser example

--- src/phaser.ts
+++ src/phaser.ts
@@ -1,44 +1,85 @@
-import Phaser, { BlendModes } from 'phaser';
+import Phaser from 'phaser';
+import DummyWalker from './dummy-walker';
+import FPSCounter from './fps-counter';
+
+function range(n: number): Array<number> {
+  return [...new Array<number>(n)].map((_, i) => i);
+}
+
+const fpsCounter = new FPSCounter(128);
+
+const avatarCount = parseInt(
+  new URLSearchParams(location.search).get('count') ?? '128'
+);
 
 class Example extends Phaser.Scene {
-  public preload(): void {
-    this.load.setBaseURL('https://labs.phaser.io');
+  private avatars: DummyWalker<Phaser.Types.Physics.Arcade.ImageWithDynamicBody>[] =
+    [];
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+  public preload(): void {
+    this.load.image('background', '/assets/test-background.png');
+    this.load.image('character', '/assets/necromant.png');
   }
 
   public create(): void {
-    this.add.image(400, 300, 'sky');
+    this.add.image(128, 128, 'background');
 
-    const particles = this.add.particles(0, 0, 'red', {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: BlendModes.ADD,
+    this.avatars = range(avatarCount).map(() => {
+      const sprite = this.physics.add.image(128, 128, 'character');
+
+      const avatar = new DummyWalker(sprite);
+      avatar.position.x = Math.floor(Math.random() * 256);
+      avatar.position.y = Math.floor(Math.random() * 256);
+
+      return avatar;
     });
+  }
 
-    const logo = this.physics.add.image(400, 100, 'logo');
+  public update(time: number, delta: number): void {
+    // Run simulation
+    this.avatars.forEach((avatar) => {
+      avatar.update(delta / 1000);
+      avatar.sprite.x = avatar.position.x | 0;
+      avatar.sprite.y = avatar.position.y | 0;
+      avatar.sprite.z = avatar.position.y;
+    });
+    // console.log(time);
+    /*
+      phaser.js:80317 1524.8
+      phaser.js:80317 1541.4
+      phaser.js:80317 1558.1
+      phaser.js:80317 1574.7
+      phaser.js:80317 1591.4
+      phaser.js:80317 1608.1
+      phaser.js:80317 1624.8
+      phaser.js:80317 1641.5
+      phaser.js:80317 1658.1
+      phaser.js:80317 1674.8
+      phaser.js:80317 1691.4
+      phaser.js:80317 1708.1
+    */
+    // Update FPS counter
+    // TODO: time is ms since start, so in theory we don't need manipulate it
+    // but the FPS counter always writes 0 :( // no the performance is better than that :D
+    fpsCounter.pushMillis(time);
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    particles.startFollow(logo);
+    const counterDiv = document.querySelector('#fps');
+    if (counterDiv) counterDiv.innerHTML = `FPS: ${fpsCounter.averageFps | 0}`;
   }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  backgroundColor: 'red',
+  width: 256,
+  height: 256,
   scene: Example,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { x: 0, y: 200 },
+      gravity: { x: 0, y: 0 },
     },
   },
 };
 
-export const game_123 = new Phaser.Game(config);
+new Phaser.Game(config);
