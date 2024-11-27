5d610c306217f66ec7b2bd7bbdb68581cf1adb9c
Tamás Gálffy <ezittgtx@gmail.com>
2024-11-27 19:14:28 +0100

    serve static

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, StreamableFile } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
+const DIST_ROOT = join(__dirname, '../../../dist/')
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  public getHello(): string {
-    return this.appService.getHello();
+  public getRoot(): StreamableFile {
+    return new StreamableFile(
+      createReadStream(join(DIST_ROOT, '/index.html')),
+      { type: 'text/html' }
+    )
+  }
+
+  @Get('threejs')
+  public getThreejs(): StreamableFile {
+    return new StreamableFile(
+      createReadStream(join(DIST_ROOT, '/threejs.html')),
+      { type: 'text/html' }
+    )
   }
 }
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+const DIST_ROOT = join(__dirname, '../../../dist/')
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(DIST_ROOT);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
