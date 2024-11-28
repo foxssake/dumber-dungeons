804de0430c8b182b1bc235394d628a350ae3a6ee
Tamás Gálffy <ezittgtx@gmail.com>
2024-11-28 20:44:34 +0100

    failed html plugin attempt

--- packages/client/scripts/bundle.ts
+++ packages/client/scripts/bundle.ts
@@ -1,6 +1,33 @@
-await Bun.build({
-  entrypoints: [
-    './src/views/threejs/page.tsx'
-  ],
-  outdir: './dist/'
-})
+import html from 'bun-plugin-html';
+import { watch } from 'fs';
+import { join, resolve } from 'path';
+
+const srcRoot = resolve(join(import.meta.dir, '/../src/'))
+const isDev = process.env.DEV == 'true'
+
+async function bundle() {
+  const result = await Bun.build({
+    entrypoints: [
+      './src/views/threejs/index.html'
+      // './src/views/threejs/page.tsx'
+    ],
+    outdir: './dist/',
+    plugins: [
+      html()
+    ]
+  })
+
+  result.logs.forEach(log => console.log(log))
+}
+
+
+if (isDev) {
+  const watcher = watch(srcRoot, { recursive: true }, () => bundle())
+  bundle()
+
+  process.on('beforeExit', () => {
+    watcher.close()
+  })
+} else {
+  await bundle();
+}
