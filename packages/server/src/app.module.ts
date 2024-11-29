51ce54a679f066eadb08f7a711302a7d439e213f
Tamás Gálffy <ezittgtx@gmail.com>
2024-11-29 15:20:44 +0100

    backend read from dist

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ViewService } from './view.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ViewService],
 })
 export class AppModule {}
