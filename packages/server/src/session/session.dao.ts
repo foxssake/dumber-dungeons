3961b73f802b137d221e3cbac460b69019a42778
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-04 12:46:07 +0100

    session svc + tests

---
+++ packages/server/src/session/session.dao.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+import { BaseDAO } from "src/base.dao";
+import type { Session } from "./session";
+
+@Injectable()
+export class SessionDAO extends BaseDAO<Session, string> {
+
+};
