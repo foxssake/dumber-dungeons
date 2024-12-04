fe73f0330847c75f257d3923504551b8187d68eb
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-04 12:46:07 +0100

    session svc + tests

---
+++ packages/server/src/session/session.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import type { SessionDAO } from "./session.dao";
+import { type Session, SessionStatus } from "./session";
+import type { IDGenerator } from "src/id.generator";
+import { assert } from '../assert';
+
+@Injectable()
+export class SessionService {
+  constructor(
+    private readonly sessionDAO: SessionDAO,
+    private readonly idGenerator: IDGenerator
+  ) {}
+
+  public create(): Session {
+    const session: Session = {
+      id: this.idGenerator.forSession(),
+      status: SessionStatus.IN_LOBBY,
+      participants: []
+    };
+
+    this.sessionDAO.save(session);
+
+    return session;
+  }
+
+  public start(session: Session): Session {
+    assert(session.status == SessionStatus.IN_LOBBY, "Session can only be started if it's already in lobby!");
+
+    session.status = SessionStatus.ACTIVE;
+    this.sessionDAO.save(session);
+
+    return session;
+  }
+
+  public find(id: string): Session | undefined {
+    return this.sessionDAO.find(id);
+  }
+}
---
+++ packages/server/test/spec/session/session.service.spec.ts
@@ -0,0 +1,93 @@
+import { describe, it, expect, beforeEach } from 'bun:test'
+import { SessionStatus, type Session } from 'src/session/session';
+import { createStubInstance, type SinonStubbedInstance } from 'sinon';
+import { IDGenerator } from 'src/id.generator'
+import { SessionDAO } from 'src/session/session.dao'
+import { SessionService } from 'src/session/session.service';
+
+describe('SessionService', () => {
+  let sessionDao: SinonStubbedInstance<SessionDAO>;
+  let idGenerator: SinonStubbedInstance< IDGenerator>;
+  let sessionService: SessionService
+
+  beforeEach(() => {
+    sessionDao = createStubInstance(SessionDAO);
+
+    idGenerator = createStubInstance(IDGenerator, {
+      forSession: '0000'
+    });
+
+    sessionService = new SessionService(sessionDao, idGenerator);
+  })
+
+  describe('create', () => {
+    it('should create session', () => {
+      // Given
+      const expected: Session = {
+        id: '0000',
+        status: SessionStatus.IN_LOBBY,
+        participants: []
+      };
+
+      // When
+      const actual = sessionService.create();
+
+      // Then
+      expect(actual).toEqual(expected);
+      expect(sessionDao.save.calledWith(expected)).toBeTrue();
+    })
+  })
+
+  describe('start', () => {
+    it('should start session', () => {
+      // Given
+      const session: Session = {
+        id: '0000',
+        status: SessionStatus.IN_LOBBY,
+        participants: []
+      };
+
+      // When
+      const result = sessionService.start(session);
+
+      // Then
+      expect(result.status).toBe(SessionStatus.ACTIVE);
+      expect(sessionDao.save.calledWith(result)).toBeTrue();
+    })
+
+    it('should not start non-lobby session', () => {
+      // Given
+      const session: Session = {
+        id: '0000',
+        status: SessionStatus.ACTIVE,
+        participants: []
+      }
+
+      // When + Then
+      expect(() => sessionService.start(session)).toThrow()
+    })
+  })
+
+  describe('find', () => {
+    it('should return known session', () => {
+      // Given
+      const expected = sessionService.create();
+      sessionDao.find.returns(expected)
+
+      // When
+      const actual = sessionService.find(expected.id);
+
+      // Then
+      expect(actual).toBe(expected);
+      expect(sessionDao.find.calledWith(expected.id)).toBeTrue();
+    })
+
+    it('should return undefined on unknown session', () => {
+      // When
+      const result = sessionService.find('0000');
+
+      // Then
+      expect(result).toBeUndefined();
+    })
+  })
+})
