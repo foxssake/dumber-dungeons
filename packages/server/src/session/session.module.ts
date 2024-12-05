a24210aff48dddf9848bd830eb55376ca930dce3
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-05 13:14:12 +0100

    forint

--- packages/server/src/session/session.module.ts
+++ packages/server/src/session/session.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
 import { SessionDAO } from './session.dao';
-import { UtilsModule } from '../utils/utils.module';
+import { UtilsModule } from 'src/utils/utils.module';
 
 @Module({
   providers: [SessionService, SessionDAO],
