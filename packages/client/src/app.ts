73247e6ff156b8d2fe46b178053c661d4d569c69
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-06 14:35:42 +0100

    link svc

--- packages/client/src/app.ts
+++ packages/client/src/app.ts
@@ -1,4 +1,7 @@
 import { createContainer } from 'iti';
+import { LinkService } from './link.service';
 
 // Setup DI context
-export const app = createContainer();
+export const app = createContainer()
+  .add({ linkService: new LinkService(new URL(window.location.origin)) });
+
