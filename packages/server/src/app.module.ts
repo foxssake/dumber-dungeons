db83c035d4078cd5dbf78b7a8c2ab1ebe148d7ec
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-08 20:57:53 +0100

    move dir config to module

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SessionModule } from './session/session.module';
 import { ViewService } from './view.service';
+import { PathsModule } from './paths/paths.module';
 
 @Module({
-  imports: [SessionModule],
+  imports: [SessionModule, PathsModule],
   controllers: [AppController],
   providers: [AppService, ViewService],
 })
