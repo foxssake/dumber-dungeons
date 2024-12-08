c4f9339f6f4d41d3a46b106cb3f59d0cc8bca6ec
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-08 22:47:12 +0100

    forint

--- packages/server/src/paths/paths.module.ts
+++ packages/server/src/paths/paths.module.ts
@@ -12,13 +12,8 @@ const viewsRoot = join(distRoot, 'views');
     { provide: 'PACKAGE_ROOT', useValue: packageRoot },
     { provide: 'PROJECT_ROOT', useValue: projectRoot },
     { provide: 'DIST_ROOT', useValue: distRoot },
-    { provide: 'VIEWS_ROOT', useValue: viewsRoot }
+    { provide: 'VIEWS_ROOT', useValue: viewsRoot },
   ],
-  exports: [
-    'PACKAGE_ROOT',
-    'PROJECT_ROOT',
-    'DIST_ROOT',
-    'VIEWS_ROOT'
-  ]
+  exports: ['PACKAGE_ROOT', 'PROJECT_ROOT', 'DIST_ROOT', 'VIEWS_ROOT'],
 })
 export class PathsModule {}
