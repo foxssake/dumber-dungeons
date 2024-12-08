11bd70b615ee93a246c617b062b5fedf443ecf61
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-08 22:47:12 +0100

    forint

--- packages/server/src/view.service.ts
+++ packages/server/src/view.service.ts
@@ -3,9 +3,7 @@ import { join } from 'path';
 
 @Injectable()
 export class ViewService {
-  constructor(
-    @Inject('VIEWS_ROOT') private readonly viewsRoot: string
-  ) {}
+  constructor(@Inject('VIEWS_ROOT') private readonly viewsRoot: string) {}
 
   public async render(view: string): Promise<string> {
     return Bun.file(join(this.viewsRoot, `${view}.html`)).text();
