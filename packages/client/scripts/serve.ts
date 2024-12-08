b8ae8d69c061e3b6de7ee863e4663dc0d1ec448a
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-08 22:37:02 +0100

    refactor serve

--- packages/client/scripts/serve.ts
+++ packages/client/scripts/serve.ts
@@ -1,49 +1,70 @@
-import { basename, join, resolve } from 'path';
+import { basename, join } from 'path';
 import { env } from 'process';
 import { globScan, publicDistRoot, viewRoot } from './shared';
 
 const port = env.PORT ?? 3000;
 
-// List views
-const views = (await globScan('*.html', { cwd: viewRoot })).map((path) =>
-  basename(path, '.html')
-);
-console.log('Found views', views);
-
 // Serve
+async function serveIndex(request: Request): Promise<Response | undefined> {
+  // Check path
+  if (new URL(request.url).pathname != '/')
+    return undefined;
+
+  // List views
+  const viewNames = (await globScan('*.html', { cwd: viewRoot }))
+    .map((path) => basename(path, '.html'));
+
+  const viewList = viewNames
+    .map(viewName => `<a href="/${viewName}">${viewName}</a>`)
+    .map(link => `<li>${link}</li>`)
+    .join('')
+
+  const html = `<html><body><ul>${viewList}</ul></body></html>`;
+
+  return new Response(html, { headers: [['Content-Type', 'text/html']] });
+}
+
+async function serveView(request: Request): Promise<Response | undefined> {
+  // Check if view exists
+  const viewName = new URL(request.url).pathname.split('/')[1];
+  const view = Bun.file(join(viewRoot, `${viewName}.html`));
+
+  if (!await view.exists())
+    return undefined;
+
+  return new Response(view);
+}
+
+async function serveAsset(request: Request): Promise<Response | undefined> {
+  const assetPath = new URL(request.url).pathname;
+  const asset = Bun.file(join(publicDistRoot, assetPath));
+
+  if (!await asset.exists())
+    return undefined;
+
+  return new Response(asset);
+}
+
+function serveNotFound(request: Request): Response {
+  const path = new URL(request.url).pathname;
+
+  console.error('Not found:', path);
+  return new Response(`Not found: ${path}`, { status: 404 });
+}
+
 Bun.serve({
   port,
   async fetch(req) {
-    const url = new URL(req.url);
-    const urlPath = url.pathname.slice(1); // Remove '/' prefix
-
     if (req.method != 'GET') {
       return new Response('', { status: 405 });
     }
-
-    // Check if it's index
-    if (urlPath == '') {
-      const html = `<html><body>${views
-        .map((view) => `<a href="/${view}">${view}</a>`)
-        .join('\n')}</body></html>`;
-
-      return new Response(html, { headers: [['Content-Type', 'text/html']] });
-    }
-
-    // Check if view exists
-    const view = Bun.file(join(viewRoot, `${urlPath}.html`));
-    if (await view.exists()) {
-      return new Response(view);
-    }
-
-    // Check if asset exists
-    const asset = Bun.file(join(publicDistRoot, url.pathname));
-    if (await asset.exists()) {
-      return new Response(asset);
-    }
-
-    console.error('Not found:', url.pathname);
-    return new Response(`Not found: ${url.pathname}`, { status: 404 });
+    
+    return (
+      (await serveIndex(req)) ??
+      (await serveView(req)) ??
+      (await serveAsset(req)) ??
+      serveNotFound(req)
+    );
   },
 });
 
