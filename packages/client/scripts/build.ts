f0606e26317e515ed917acf0edb1fad0166b2e87
Faragó Tamás <ocsi0520@gmail.com>
2024-12-12 21:40:28 +0100

    chore build.ts

--- packages/client/scripts/build.ts
+++ packages/client/scripts/build.ts
@@ -4,14 +4,16 @@ import { basename, dirname, extname, join, relative, resolve } from 'path';
 const packageDir = resolve(join(dirname(import.meta.path), '..'));
 const distDir = resolve(join(packageDir, 'dist'));
 
-const htmlTemplate = await Bun.file(join(packageDir, 'src/template.html')).text();
+const htmlTemplate = await Bun.file(
+  join(packageDir, 'src/template.html')
+).text();
 
-const writeHtml = async (sourcePath: string): Promise<void> => {
-  const relativePath = relative(distDir, sourcePath);
-  const resultName = basename(sourcePath, extname(sourcePath));
+const writeHtml = async (builtEntryPath: string): Promise<void> => {
+  const relativePath = relative(distDir, builtEntryPath);
+  const entryName = basename(builtEntryPath, extname(builtEntryPath));
 
-  const htmlPath = join(dirname(sourcePath), `${resultName}.html`);
-  const htmlText = htmlTemplate.replaceAll('{main}', relativePath);
+  const htmlPath = join(dirname(builtEntryPath), `${entryName}.html`);
+  const htmlText = htmlTemplate.replaceAll('{scriptPath}', relativePath);
 
   await Bun.write(htmlPath, htmlText);
 };
@@ -22,16 +24,18 @@ const build = async (): Promise<void> => {
     outdir: distDir,
     plugins: [],
   });
-  // TODO: remove logs
-  console.log(response);
-  if(response.success)
-    console.log('succesfully done at', new Date().toISOString());
-  else
-    console.log(response.logs);
 
-  await Promise.all(response.outputs
-    .filter(output => output.kind == 'entry-point')
-    .map(output => writeHtml(output.path)));
+  if (response.success)
+    console.log('succesfully done at', new Date().toISOString());
+  else console.log(response.logs);
+
+  await Promise.all(
+    response.outputs
+      .filter((output) => output.kind == 'entry-point')
+      .map((entryPoint) => writeHtml(entryPoint.path))
+  ).catch((e: unknown) => {
+    console.log('Could not write html files:', e);
+  });
 };
 
 const isWatching = process.argv.includes('--watch');
