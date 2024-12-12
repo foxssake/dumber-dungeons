fcae61ad7f54bf607820aa209b5867cd92789b84
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-12 18:15:16 +0100

    generate html

--- packages/client/scripts/build.ts
+++ packages/client/scripts/build.ts
@@ -1,9 +1,25 @@
 import { watch } from 'fs';
+import { basename, dirname, extname, join, relative, resolve } from 'path';
+
+const packageDir = resolve(join(dirname(import.meta.path), '..'));
+const distDir = resolve(join(packageDir, 'dist'));
+
+const htmlTemplate = await Bun.file(join(packageDir, 'src/template.html')).text();
+
+const writeHtml = async (sourcePath: string): Promise<void> => {
+  const relativePath = relative(distDir, sourcePath);
+  const resultName = basename(sourcePath, extname(sourcePath));
+
+  const htmlPath = join(dirname(sourcePath), `${resultName}.html`);
+  const htmlText = htmlTemplate.replaceAll('{main}', relativePath);
+
+  await Bun.write(htmlPath, htmlText);
+}
 
 const build = async (): Promise<void> => {
   const response = await Bun.build({
     entrypoints: ['src/index.tsx'],
-    outdir: 'dist',
+    outdir: distDir,
     plugins: [],
   });
   // TODO: remove logs
@@ -12,6 +28,10 @@ const build = async (): Promise<void> => {
     console.log('succesfully done at', new Date().toISOString());
   else
     console.log(response.logs);
+
+  await Promise.all(response.outputs
+    .filter(output => output.kind == 'entry-point')
+    .map(output => writeHtml(output.path)));
 };
 
 const isWatching = process.argv.includes('--watch');
