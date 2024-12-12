729f3b12fee088f73513768a0a0e20c276bfc798
Faragó Tamás <ocsi0520@gmail.com>
2024-12-12 21:53:32 +0100

    drop eslint warnings

--- packages/client/eslint.config.mjs
+++ packages/client/eslint.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 
-import {
-  config as mergeConfigs,
-} from 'typescript-eslint';
+import { config as mergeConfigs } from 'typescript-eslint';
+// eslint-disable-next-line no-restricted-imports
 import sharedConfig from '../shared/eslint.config.mjs';
 
-export default mergeConfigs(
-  sharedConfig
-)
+export default mergeConfigs(sharedConfig, {
+  rules: {
+    'import-x/no-nodejs-modules': 'off',
+  },
+});
