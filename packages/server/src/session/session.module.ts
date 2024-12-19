bf45cd33cf8b563bffe01123f8322579da42bc81
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-19 14:43:00 +0100

    fxs

--- packages/server/src/session/session.module.ts
+++ packages/server/src/session/session.module.ts
@@ -3,10 +3,9 @@ import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
 import { SessionDAO } from './session.dao';
 import { UtilsModule } from 'src/utils/utils.module';
-import { ParticipantDAO } from './participant.dao';
 
 @Module({
-  providers: [SessionService, SessionDAO, ParticipantDAO],
+  providers: [SessionService, SessionDAO],
   controllers: [SessionController],
   imports: [UtilsModule],
 })
