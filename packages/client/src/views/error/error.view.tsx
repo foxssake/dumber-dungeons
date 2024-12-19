96fe2000892f25e94c49bf4beb896e5636a21cca
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-19 19:47:34 +0100

    cleanup

--- packages/client/src/views/error/error.view.tsx
+++ packages/client/src/views/error/error.view.tsx
@@ -3,7 +3,7 @@ import type { JSX } from 'react';
 export function ErrorView(props: { error: Error }): JSX.Element {
   return (
     <>
-      <h1>Error - {props.error.message}</h1>
+      <h1>{props.error.message || 'Error'}</h1>
       <p>Stack trace:</p>
       <pre>{String(props.error.stack)}</pre>
     </>
