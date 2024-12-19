626e2162d78c80baf9db319edf01bcc3be802fb4
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-19 14:43:53 +0100

    feat: Participant management (#45)
    Closes #24

--- packages/server/src/session/session.service.ts
+++ packages/server/src/session/session.service.ts
@@ -3,6 +3,13 @@ import { SessionDAO } from './session.dao';
 import { type Session, SessionStatus } from './session';
 import assert from 'node:assert';
 import { IDGenerator } from 'src/utils/id.generator';
+import type { Participant } from './participant';
+import { verify } from '@dumber-dungeons/shared/src/verify';
+
+export type JoinSessionOptions = {
+  name?: string;
+  isDisplay?: boolean;
+};
 
 @Injectable()
 export class SessionService {
@@ -23,10 +30,39 @@ export class SessionService {
     return session;
   }
 
-  public async start(session: Session): Promise<Session> {
-    assert(
+  public async join(
+    session: Session,
+    options?: JoinSessionOptions
+  ): Promise<Participant> {
+    assert(await this.hasSession(session), 'Unknown session!');
+
+    verify(
       session.status == SessionStatus.IN_LOBBY,
-      "Session can only be started if it's already in lobby!"
+      "Session can only be joined if it's in lobby!"
+    );
+
+    // Create and save participant
+    const participant: Participant = {
+      id: this.idGenerator.forParticipant(),
+      name: options?.name ?? '',
+      isReady: false,
+      isDisplay: options?.isDisplay ?? false,
+      authToken: this.idGenerator.forAuth(),
+    };
+
+    // Save updated session
+    session.participants.push(participant);
+    await this.sessionDAO.save(session);
+
+    return participant;
+  }
+
+  public async start(session: Session): Promise<Session> {
+    assert(await this.hasSession(session), 'Unknown session!');
+
+    verify(
+      session.status == SessionStatus.IN_LOBBY,
+      "Session can only be started if it's in lobby!"
     );
 
     session.status = SessionStatus.ACTIVE;
@@ -37,5 +73,9 @@ export class SessionService {
 
   public find(id: string): Promise<Session | undefined> {
     return this.sessionDAO.find(id);
+  }
+
+  private hasSession(session: Session): Promise<boolean> {
+    return this.sessionDAO.has(session.id);
   }
 }
